e089bb2e16c61d3fc52935aa79d6677cf96398f2
Ldltorre14 <lopdlt.learning@gmail.com>
2026-04-24 23:02:06 -0600

    feat: Improved CID class

--- src/brain/cid_core.py
+++ src/brain/cid_core.py
@@ -1,6 +1,8 @@
 from brain.sentence_encoder import CommandLanguageModel 
-from config import device, model_name
-import torch
+from brain.rl_agent.ppo_agent import PPOAgent
+from memory.omni_memory import OmniMemoryModule
+
+import torch 
 import logging 
 from collections import deque
 import numpy as np
@@ -10,14 +12,29 @@ logger = logging.getLogger("cid_core")
 
 
 class CID:
-    def __init__(self):
+    def __init__(self, 
+                 encoder_model_name: str, 
+                 in_embedding_dim: int = 768,
+                 out_embedding_dim: int = 3,
+                 device: str = "cuda" if torch.cuda.is_available() else "cpu",
+                 backend: str = "torch",
+                 model_kwargs: dict = {"dtype": "float32"}):
+        
+        self.long_memory = OmniMemoryModule(
+            
+        )
+
         self.encoder = CommandLanguageModel(
             device = device,
-            model_name = model_name,
-            backend = "torch", 
-            model_kwargs = {"dtype": "float32"}
+            model_name = encoder_model_name,
+            backend = backend, 
+            model_kwargs = model_kwargs
         )
-        self.input_buffer = deque(maxlen=10)
+        
+        self.agent = PPOAgent(
+            in_embed_dim = in_embedding_dim,
+            out_embed_di = out_embedding_dim
+        )
         
         self._start_up()
     
@@ -27,13 +44,14 @@ class CID:
     
     def _read_command(self, command: str):
         encoded = self.encoder.encode_command(command=command)
-        self.input_buffer.append(encoded)
+        return encoded
     
 
     def run_command(self, command: str):
-        self._read_command(command=command)
+        state_embedding = self._read_command(command=command)
+        pred_action = self.agent.select_action(state_embedding=state_embedding)
 
-        return self.input_buffer
+        return pred_action
 
 
 
